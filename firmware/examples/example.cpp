4426b012748afb5173f18145e315fe06554a6b1b
Eduardo Marín <eduardo.marin.contractor@dev.bbva.com>
2015-12-29 08:05:32 +0100

    example cleaned

--- firmware/examples/example.cpp
+++ firmware/examples/example.cpp
@@ -16,18 +16,14 @@ void setup() {
 void loop() { 
 
     float rzero = gasSensor.getRZero();
-    digitalWrite(led,HIGH);      // turn the ledPin on
-    delay(100);                      // stop the program for some time
-    digitalWrite(led, LOW);       // turn the ledPin off
-    delay(100);  // stop the program for some time
-
+    digitalWrite(led,HIGH);             // turn the ledPin on
+    delay(100);                         // stop the program for some time
+    digitalWrite(led, LOW);             // turn the ledPin off
+    delay(100);                         // stop the program for some time
 
     int co2_ppm = gasSensor.getPPM();
     int ppm = co2_ppm / 4;
-    Spark.variable("ppm", &ppm, INT);
-    //Vrl = val * ( 5.00 / 1024.0  );      // V
-    //Rs = 20000 * ( 5.00 - Vrl) / Vrl ;   // Ohm 
-    //ratio =  Rs/Ro;                      
+    Spark.variable("ppm", &ppm, INT);                    
 
     delay(10000);
 }
