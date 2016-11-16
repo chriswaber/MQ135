621995b907e75bef1a76cb35146139fe44d34c6e
chriswaber <chriswaber@gmail.com>
2016-11-16 23:32:38 +0100

    Update MQ135.h

--- firmware/MQ135.h
+++ firmware/MQ135.h
@@ -19,7 +19,7 @@ v1.0 - First release
 /// The load resistance on the board
 #define RLOAD 10.0
 /// Calibration resistance at atmospheric CO2 level
-#define RZERO 700.63
+#define RZERO 419.00
 /// Parameters for calculating ppm of CO2 from sensor resistance
 #define PARA 116.6020682
 #define PARB 2.769034857
